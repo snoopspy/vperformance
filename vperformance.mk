0de4ea9b535054d8ee971d7df6e092fe8edf1998
gilgil <gilgil@gilgil.net>
2015-04-03 21:23:24 +0900

    Change '_PATH' > '_DIR'

--- vperformance.mk
+++ vperformance.mk
@@ -6,5 +6,5 @@ CPPFLAGS += -std=c++11
 #
 # vperformance
 #
-VPERFORMANCE_PATH := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
-CPPFLAGS += -I$(VPERFORMANCE_PATH)/src
+VPERFORMANCE_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+CPPFLAGS += -I$(VPERFORMANCE_DIR)/src
